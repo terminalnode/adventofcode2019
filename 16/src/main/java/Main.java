2441028f3a6d56835b2239dc4857f4066c68fc37
Alexander <2683344+terminalnode@users.noreply.github.com>
2019-12-25 00:49:26 +0100

    java: day 16, part 1 solved

--- 16/src/main/java/Main.java
+++ 16/src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
@@ -12,11 +13,49 @@ class Main {
         partOne("test1", 24176176);
         partOne("test2", 73745418);
         partOne("test3", 52432133);
-        partOne("input", 0);
+        partOne("input", 29795507);
+        partTwo("test1", 0);
+    }
+
+    public static void partTwo(String fileName, int shouldBe) throws IOException {
+        System.out.println("part two start");
+        int[] input = repeatInput(getInput(fileName));
+        /*
+         * Way way way waaaaaay too slow
+        for (int i = 0; i < 100; i++) {
+            phaseTransformation(input);
+        }
+        */
     }
 
     public static void partOne(String fileName, int shouldBe) throws IOException {
         int[] input = getInput(fileName);
+        for (int i = 0; i < 100; i++) {
+            phaseTransformation(input);
+        }
+        int result = intifyArray(input, 0);
+        String resultString = result == shouldBe ? "Correct!" : "Wrong!";
+        System.out.printf("Part one (for %s): %s (%s)\n", fileName, result, resultString);
+    }
+
+    private static void phaseTransformation(int[] input) {
+        int length = input.length;
+        for (int i = 0; i < length; i++) {
+            Iterator<Integer> iterator = getRepeatingPattern(i, length);
+            int newValue = 0;
+            for (int j : input) {
+                newValue += j * iterator.next();
+            }
+            input[i] = Math.abs(newValue) % 10;
+        }
+    }
+
+    private static int intifyArray(int[] input, int offset) {
+        StringBuilder output = new StringBuilder();
+        for (int i = offset; i < offset + 8; i++) {
+            output.append(input[i]);
+        }
+        return Integer.parseInt(output.toString());
     }
 
     private static Iterator<Integer> getRepeatingPattern(int forPosition, int length) {
@@ -35,6 +74,13 @@ class Main {
 
         result.next(); // skip first value
         return result;
+    }
+
+    private static int[] repeatInput(int[] input) {
+        return Stream.generate(() -> input)
+            .flatMapToInt(Arrays::stream)
+            .limit(10000 * input.length)
+            .toArray();
     }
 
     private static int[] getInput(String fileName) throws IOException {
